603d3fc551d25e437dd83dc4b72860ef9d30a601
João Pedro Miranda Marques <joaopedromiranda2003@gmail.com>
2024-11-28 16:50:50 -0300

    Ajustes para o css funcionar

--- server.js
+++ server.js
@@ -11,21 +11,20 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Rota para /derivada
-app.get('/derivada', (req, res) => {
-    res.send('Você acessou a rota /derivada');
-});
-
-// Rota para /integral
-app.get('/integral', (req, res) => {
-    res.send('Você acessou a rota /integral');
-});
-
-// Rota para /limite
+// Rotas específicas
 app.get('/limite', (req, res) => {
-    res.send('Você acessou a rota /limite');
+    res.sendFile(path.join(__dirname, 'public', 'limite.html'));
 });
 
+app.get('/derivada', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'derivada.html'));
+});
+
+app.get('/integral', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'integral.html'));
+});
+
+// Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
