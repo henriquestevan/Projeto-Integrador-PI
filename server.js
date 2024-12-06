ea46625b63c9306e0d3f972fac881f9f790b9418
João Pedro Miranda Marques <joaopedromiranda2003@gmail.com>
2024-12-06 14:06:22 -0300

    altercoes necessarias para funcionar o css

--- server.js
+++ server.js
@@ -3,30 +3,23 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Configurar o diretório público para servir arquivos estáticos
+// Configura o diretório público para servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Rota para a página inicial
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 // Rotas específicas
 app.get('/limite', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'Limite', 'limite.html'));
+    res.sendFile(path.join(__dirname, 'public', 'limite.html'));
 });
 
 app.get('/derivada', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'Derivada', 'derivada.html'));
+    res.sendFile(path.join(__dirname, 'public', 'derivada.html'));
 });
 
 app.get('/integral', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'integral', 'integral.html'));
+    res.sendFile(path.join(__dirname, 'public', 'integral.html'));
 });
 
-
-
-// Iniciar o servidor
+// Inicializa o servidor
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
