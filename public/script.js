457f6e332ac5cb8740be7f97eb769bde2f27ff31
João Pedro Miranda Marques <joaopedromiranda2003@gmail.com>
2024-12-06 21:11:19 -0300

    commit

--- public/script.js
+++ public/script.js
@@ -19,6 +19,31 @@ app.get('/integral', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'integral.html'));
 });
 
+// Rotas para as páginas de teoria e prática
+app.get('/TeoriaDerivada', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'TeoriaDerivada.html'));
+});
+
+app.get('/TeoriaIntegral', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'TeoriaIntegral.html'));
+});
+
+app.get('/TeoriaLimites', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'TeoriaLimites.html'));
+});
+
+app.get('/PraticaDerivada', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'PraticaDerivada.html'));
+});
+
+app.get('/PraticaIntegral', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'PraticaIntegral.html'));
+});
+
+app.get('/PraticaLimite', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'PraticaLimite.html'));
+});
+
 // Rota para lidar com erros 404
 app.use((req, res) => {
     res.status(404).send('Página não encontrada');
